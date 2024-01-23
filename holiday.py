737413e1ccaa1e75ae3004edc80ffeef6b270fca
cfff226 <cazziednb@outlook.com>
2024-01-23 18:52:27 +0000

    Create a function called plane cost which will calculate the cost of the flight - Pass in the arguments list of cities, city flight and plane cost

--- holiday.py
+++ holiday.py
@@ -20,13 +20,13 @@ while True:
         print("You have entered incorrectly")
         continue
     else:
-        print(f"\nGreat, you have chosen to fly to {city_flight.capitalize}")
+        print(f"\nGreat, you have chosen to fly to {city_flight.capitalize()}")
         break
 
 
 # Function which requests the number of nights that the user would like to book a hotel for
 def number_of_nights():
-    num_nights = input("\nPlease input the number of nights that you wish to stay: \n")
+    num_nights = input("\nPlease input the number of nights that you wish to stay: ")
     try:
         num_nights = int(num_nights)
         if num_nights <= 0:
@@ -46,7 +46,7 @@ num_nights = number_of_nights()
 # Function which requests the number of says that the user would like to rent a car for
 def car_rental_days():
     rental_days = input(
-        "\nPlease input the number of days that you would like to hire a car for: \n"
+        "\nPlease input the number of days that you would like to hire a car for: "
     )
     try:
         rental_days = int(rental_days)
@@ -61,7 +61,7 @@ def car_rental_days():
         car_rental_days()
 
 
-rental_days = car_rental_days
+rental_days = car_rental_days()
 
 
 # Function which calculates the cost of the hotel per night and outputs to the user
@@ -81,6 +81,8 @@ hotel_cost(num_nights, daily_hotel_cost, list_of_cities, city_flight)
 def car_rental_cost(rental_days, list_of_cities, daily_car_rental, city_flight):
     index = list_of_cities.index(city_flight)
     daily_car_rental = daily_car_rental[index]
+    print(rental_days)
+    print(daily_car_rental)
     car_total = daily_car_rental * rental_days
     print(
         f"\nThe total cost of the car rental in {list_of_cities[index].capitalize()} would be £{car_total}"
@@ -88,3 +90,6 @@ def car_rental_cost(rental_days, list_of_cities, daily_car_rental, city_flight):
 
 
 car_rental_cost(rental_days, list_of_cities, daily_car_rental, city_flight)
+
+
+def plane_cost(list_of_citites, city_flight, plane_cost):
