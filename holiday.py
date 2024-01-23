684c8ab7dcb79dcd25e3bf89e5299f76bc3e947f
cfff226 <cazziednb@outlook.com>
2024-01-23 17:54:32 +0000

    Add hotel cost function which will calculate the total cost of the hotel - Pass in the arguments of number of nights, daily hotel cost, list of cities and the city flight - Apply the lower method to the city flight - Find the index of the city flight in the list of cities - Multiply the daily hotel cost by the number of nights and assign this to a variable - Output this total to the user

--- holiday.py
+++ holiday.py
@@ -13,7 +13,9 @@ daily_hotel_cost = [78, 82, 125, 110, 90]
 # Request the city choice from the user
 while True:
     print("\n\t\t\tWelcome to Yourtrip!")
-    city_flight = input("\nPlease type either 'Berlin', 'Amsterdam', 'Zurich', 'Paris' or 'Budapest: ")
+    city_flight = input(
+        "\nPlease type either 'Berlin', 'Amsterdam', 'Zurich', 'Paris' or 'Budapest: "
+    )
     if city_flight.lower() not in list_of_cities:
         print("You have entered incorrectly")
         continue
@@ -21,6 +23,7 @@ while True:
         city_flight = city_flight.capitalize()
         print(f"\nGreat, you have chosen to fly to {city_flight}")
         break
+
 
 # Function which requests the number of nights that the user would like to book a hotel for
 def number_of_nights():
@@ -33,8 +36,10 @@ def number_of_nights():
     except ValueError:
         print("You have entered incorrectly")
         number_of_nights()
-    
-    return num_nights
+
+
+num_nights = number_of_nights()
+
 
 # Function which requests the number of says that the user would like to rent a car for
 def car_rental_days():
@@ -53,4 +58,15 @@ def car_rental_days():
     return rental_days
 
 
+# Function which calculates the cost of the hotel per night and outputs to the user
+def hotel_cost(num_nights, daily_hotel_cost, list_of_cities, city_flight):
+    city_flight = city_flight.lower()
+    index = list_of_cities.index(city_flight)
+    daily_hotel_cost = daily_hotel_cost[index]
+    hotel_total = daily_hotel_cost * num_nights
+    print(
+        f"\n- The total cost of the hotel in {list_of_cities[index]} would be £{hotel_total}"
+    )
 
+
+hotel_cost(num_nights, daily_hotel_cost, list_of_cities, city_flight)
