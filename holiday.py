e5b54cbc5147127bbf11668c94acc27d348f553f
cfff226 <cazziednb@outlook.com>
2024-01-23 17:10:25 +0000

    Create a function called number of nights - Add an input function to request the number of nights that the user wishes to stay and assign this to a variable - Add a try statement - Cast the users input into an integer and re assign it to itself

--- holiday.py
+++ holiday.py
@@ -1,6 +1,8 @@
+import math
+
 # Program which calculates the total cost of a holiday for a user
 
-list_of_cities = ["berlin", "amsterdam", "zurich", "paris", "budapest"]
+list_of_cities = ["berlin", "amsterdam", "zurich", "paris", "budapest: "]
 
 plane_cost = [89, 65, 110, 95, 87]
 
@@ -8,8 +10,10 @@ daily_car_rental = [39, 45, 86, 98, 72]
 
 # Request the city choice from the user
 while True:
-    print("\nWelcome to Yourtrip!")
-    city_flight = input("\nPlease type either 'Berlin', 'Amsterdam', 'Zurich', 'Paris' or 'Budapest ")
+    print("\n\t\t\tWelcome to Yourtrip!")
+    city_flight = input(
+        "\nPlease type either 'Berlin', 'Amsterdam', 'Zurich', 'Paris' or 'Budapest "
+    )
     if city_flight.lower() not in list_of_cities:
         print("You have entered incorrectly")
         continue
@@ -17,3 +21,9 @@ while True:
         city_flight = city_flight.capitalize()
         print(f"\nGreat, you have chosen to fly to {city_flight}")
         break
+
+# Function which requests the number of nights that the user would like to book a hotel for
+def number_of_nights():
+    num_nights = input("\nPlease input the number of nights that you wish to stay: \n")
+    try:
+        num_nights = int(num_nights)
