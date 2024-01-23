5e6fa185d67ac9f5173f863941169b0599add481
cfff226 <cazziednb@outlook.com>
2024-01-23 16:57:35 +0000

    Add while loop to run while true - Print a welcome message to the user - Request the user input for their choice of city - Add if statement to check if the user input with the lower method applied is not in the list of cities - Print a message to show that the user has entered incorrectly

--- holiday.py
+++ holiday.py
@@ -1,3 +1,10 @@
 # Program which calculates the total cost of a holiday for a user
 
 list_of_cities = ["berlin", "amsterdam", "zurich", "paris", "budapest"]
+
+# Request the city choice from the user
+while True:
+    print("\nWelcome to Yourtrip!")
+    city_flight = input("\nPlease type either 'Berlin', 'Amsterdam', 'Zurich', 'Paris' or 'Budapest ")
+    if city_flight.lower() not in list_of_cities:
+        print("You have entered incorrectly")
